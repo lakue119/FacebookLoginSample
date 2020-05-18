ef2c53d9b05f504a78819e786ff5a09357ef02b2
Lakue <audfkr0808@naver.com>
2020-05-19 01:05:51 +0900

    facebook login sample

---
+++ app/src/main/java/com/lakue/facebooksample/MainActivity.java
@@ -0,0 +1,73 @@
+package com.lakue.facebooksample;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
+import android.os.Bundle;
+import android.util.Base64;
+import android.util.Log;
+
+import com.facebook.CallbackManager;
+import com.facebook.login.widget.LoginButton;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Arrays;
+
+public class MainActivity extends AppCompatActivity {
+
+    private LoginButton btn_facebook_login;
+
+    private LoginCallback mLoginCallback;
+    private CallbackManager mCallbackManager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        getHashKey();
+
+        mCallbackManager = CallbackManager.Factory.create();
+        mLoginCallback = new LoginCallback();
+
+        btn_facebook_login = (LoginButton) findViewById(R.id.btn_facebook_login);
+        btn_facebook_login.setReadPermissions(Arrays.asList("public_profile", "email"));
+        btn_facebook_login.registerCallback(mCallbackManager, mLoginCallback);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        mCallbackManager.onActivityResult(requestCode, resultCode, data);
+        super.onActivityResult(requestCode, resultCode, data);
+
+        //여기에다가 페이지 이동 코드를 넣어주면 됩니다.
+        Intent intent = new Intent(this, Main2Activity.class);
+        startActivity(intent);
+    }
+
+    private void getHashKey(){
+        PackageInfo packageInfo = null;
+        try {
+            packageInfo = getPackageManager().getPackageInfo(getPackageName(), PackageManager.GET_SIGNATURES);
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
+        if (packageInfo == null)
+            Log.e("KeyHash", "KeyHash:null");
+
+        for (Signature signature : packageInfo.signatures) {
+            try {
+                MessageDigest md = MessageDigest.getInstance("SHA");
+                md.update(signature.toByteArray());
+                Log.d("KeyHash", Base64.encodeToString(md.digest(), Base64.DEFAULT));
+            } catch (NoSuchAlgorithmException e) {
+                Log.e("KeyHash", "Unable to get MessageDigest. signature=" + signature, e);
+            }
+        }
+    }
+
+}
